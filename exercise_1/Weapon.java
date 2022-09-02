fd4e081d9d2f48eedbf18f5e6b14592d58e58597
rizjake.p <112447579+pinedarizjake@users.noreply.github.com>
2022-09-02 16:43:21 +0800

    Create Weapon.java

---
+++ exercise_1/Weapon.java
@@ -0,0 +1,14 @@
+public class Weapon {
+	
+	public String Name;
+	public int Damage;
+	public double Weight;
+	public String Rarity;
+	public String Description;
+
+
+	    public void WeaponDescription(){
+	        System.out.println(Description);
+	    }
+
+}
