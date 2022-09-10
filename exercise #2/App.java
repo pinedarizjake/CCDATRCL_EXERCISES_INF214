50ace5e7ff67c476a48d37834c111ffc4373c7b4
rizjake.p <112447579+pinedarizjake@users.noreply.github.com>
2022-09-10 16:56:31 +0800

    Update App.java

--- exercise #2/App.java
+++ exercise #2/App.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 class Main {
     public static void main(String[] args) {
 
-        // Creates a new Linkedlist Object
+        // 1. Create your own music playlist by creating a LinkedList object
         LinkedList<String> Favlist = new LinkedList<>();
 
         // Add 5 new songs to your Playlist using the add() method
